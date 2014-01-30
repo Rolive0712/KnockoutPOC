e71e6bee5d28dc2282125b34cac1ad37bb1d814b
roshitgit <roshitrajan@gmail.com>
2014-01-30 18:01:06 +0530

    Create Index.js

---
+++ Scripts/ViewModels/Index.js
@@ -0,0 +1,20 @@
+(function () {
+
+    require(["bootstrapper"], function (bootstapper) { //load bootstrapper
+        require(["jquery"], function ($) {              //if ok, load jquery
+            require(["ViewModels/IndexScript"]);            //if ok. load IndexScript
+            $(function () {                             //register button clicks after DOM ready.
+                $('#projSearch, #northProducts, #TAPCTO').click(function (event) { //load scripts on-demand on button click
+                    event.preventDefault();
+                    if (event.target.id === "projSearch")
+                        require(["ViewModels/PTSQuickSearch"]);
+                    else if (event.target.id === "northProducts")
+                        require(["ViewModels/NorthwindSupplierProducts"]);
+                    else if (event.target.id === "TAPCTO")
+                        require(["ViewModels/TAPCTO"]);
+
+                });
+            });
+        });
+    });
+})();
