87fea5ec47619879e7e683daaa2d66709bdfcd22
roshitgit <roshitrajan@gmail.com>
2014-01-30 17:47:01 +0530

    Create TAPEntity.cs

---
+++ Models/TAPEntity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4KnockoutPOC.Models
+{
+    public class TAPEntity
+    {
+        public string TPRNbr { get; set; }
+        public string PrjNm { get; set; }
+        public string LOB { get; set; }
+        public int Age { get; set; }
+        public string DC { get; set; }
+        public int surveyver { get; set; }
+        public string Phase { get; set; }
+    }
+}
