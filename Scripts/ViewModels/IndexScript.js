828a4d3551552396d997b72c20d6304ef84ee6cf
roshitgit <roshitrajan@gmail.com>
2014-02-04 19:28:01 -0500

    Update IndexScript.js

--- Scripts/ViewModels/IndexScript.js
+++ Scripts/ViewModels/IndexScript.js
@@ -1,44 +1,40 @@
 /// <reference path="../_references.js" />              for intellisense
 
-function TestRequireJSObjects($, ui, ko, AJAX) {
-    console.log("Test output");
-    console.log("$: " + typeof ($));
-    console.log("ko: " + typeof (ko));
-    console.log("common: " + typeof (AJAX));
-    console.log("jquery downloaded confirm: " + $.trim("Roshit   "));
-}
 
-$(function () { //after dom ready, load scripts asynchronously
-    var dependencies = [
-        "Lib/jquery-ui-1.10.3.min",
-        "knockout",
-        "ViewModels/DataService",
-        "ViewModels/Logger"
-    ];
 
-    var App = App || {};
+(function () {
+    "use strict"; //allows strong typing in javascript
+    $(function () { //after dom ready, load scripts asynchronously
+        var dependencies = [
+            "Lib/jquery-ui-1.10.3.min",
+            "knockout",
+            "ViewModels/DataService",
+            "ViewModels/Logger"
+        ];
 
-    requirejs(dependencies, function (ui, ko, dataservice, logger) {
-        $('#tabs').tabs();
-        App.ViewModel = function () {
-            var self = this;
-            //1. KO computed example: Any change in the num1 or num2 computes the sum using KO compute
-            //2. No need to attach any event handlers like "onblur" to recalculate.
+        var App = App || {};
 
-            self.NUM1 = ko.observable("1");
-            self.NUM2 = ko.observable("2");
-            self.ComputedSUM = ko.computed(function () {
-                return parseInt(self.NUM1()) + parseInt(self.NUM2());
-            });
+        requirejs(dependencies, function (ui, ko, dataservice, logger) {
+            $('#tabs').tabs();
+            App.ViewModel = function () {
+                var self = this;
+                //1. KO computed example: Any change in the num1 or num2 computes the sum using KO compute
+                //2. No need to attach any event handlers like "onblur" to recalculate.
 
-            self.firstName = ko.observable("Roshit");           //static hard coded text
-            self.lastName = ko.observable("Rajan");             //static hard coded text
-            self.personInfoArray = ko.observableArray([]);      //set default blank array => used for binding data from server.
+                self.NUM1 = ko.observable("1");
+                self.NUM2 = ko.observable("2");
+                self.ComputedSUM = ko.computed(function () {
+                    return parseInt(self.NUM1()) + parseInt(self.NUM2());
+                });
 
-            self.GetPersons = function () {
-                $('#koTbody').empty();                          // clear the table tbody before binding
-                //USING MULTIPLE INLINE DEFERRED OBJECTS
-                var deferredTemplates = dataservice.loadClientTemplates("koPersonTemplate", "../ClientTemplates/_templates.htm"),
+                self.firstName = ko.observable("Roshit");           //static hard coded text
+                self.lastName = ko.observable("Rajan");             //static hard coded text
+                self.personInfoArray = ko.observableArray([]);      //set default blank array => used for binding data from server.
+
+                self.GetPersons = function () {
+                    $('#koTbody').empty();                          // clear the table tbody before binding
+                    //USING MULTIPLE INLINE DEFERRED OBJECTS
+                    var deferredTemplates = dataservice.loadClientTemplates("koPersonTemplate", "../ClientTemplates/_templates.htm"),
                                     deferredPersons = dataservice.GetPromise({
                                         url: "../Home/GetPersons",
                                         type: "GET",
@@ -46,24 +42,33 @@ $(function () { //after dom ready, load scripts asynchronously
                                         async: true
                                     });
 
-                $.when(deferredTemplates, deferredPersons).done(function (data1, personData) {
+                    $.when(deferredTemplates, deferredPersons).done(function (data1, personData) {
 
-                    var json = ko.utils.parseJson(personData[2].responseText);
-                    self.personInfoArray(json);
-                    $('#loadingIcon').hide();
-                    logger.LogSuccess("data loaded");
+                        var json = ko.utils.parseJson(personData[2].responseText);
+                        self.personInfoArray(json);
+                        $('#loadingIcon').hide();
+                        logger.LogSuccess("data loaded");
 
-                }).fail(function (jqXHR, textStatus, errorThrown) { // async failure
-                    logger.LogError(errorThrown + ":" + jqXHR.responseText);
-                });
+                    }).fail(function (jqXHR, textStatus, errorThrown) { // async failure
+                        logger.LogError(errorThrown + ":" + jqXHR.responseText);
+                    });
+                };
             };
-        };
 
-        ko.setTemplateEngine(ko.nativeTemplateEngine.instance);     // to speed up rendering. to check timings
-        ko.applyBindings(new App.ViewModel(), $('#PersonInfoSection')[0]);
+            ko.setTemplateEngine(ko.nativeTemplateEngine.instance);     // to speed up rendering. to check timings
+            ko.applyBindings(new App.ViewModel(), $('#PersonInfoSection')[0]);
+        });
+
     });
+})();
 
-});
+function TestRequireJSObjects($, ui, ko, AJAX) {
+    console.log("Test output");
+    console.log("$: " + typeof ($));
+    console.log("ko: " + typeof (ko));
+    console.log("common: " + typeof (AJAX));
+    console.log("jquery downloaded confirm: " + $.trim("Roshit   "));
+}
 
 //show loading icon to any ajax start or stop within any page
 
